bcadcdf1b9c4ab6458328e4e53ae430f3c7db8c7
Sean Dempsey <dempsey.sean@outlook.com>
2024-12-31 11:30:05 -0800

    Make observable/reactive properties for session on viewmodel base

--- ArchipelagoDebugClient/ViewModels/ViewModelBase.cs
+++ ArchipelagoDebugClient/ViewModels/ViewModelBase.cs
@@ -1,10 +1,29 @@
-﻿using ArchipelagoDebugClient.Services;
+﻿using Archipelago.MultiClient.Net;
+using ArchipelagoDebugClient.Services;
 using ReactiveUI;
+using System.Reactive.Linq;
 
 namespace ArchipelagoDebugClient.ViewModels;
 
-public class ViewModelBase(SessionProvider sessionProvider) : ReactiveObject
+public class ViewModelBase : ReactiveObject
 {
     // todo - this should be protected but can't be until MainView's functionality moves to the viewmodel
-    public readonly SessionProvider sessionProvider = sessionProvider;
+    public readonly SessionProvider sessionProvider;
+
+    private readonly ObservableAsPropertyHelper<ArchipelagoSession?> _session;
+    public ArchipelagoSession? Session => _session.Value;
+
+    private readonly ObservableAsPropertyHelper<bool> _hasSession;
+    public bool HasSession => _hasSession.Value;
+
+    public ViewModelBase(SessionProvider sessionProvider)
+    {
+        this.sessionProvider = sessionProvider;
+        _session = Observable.Concat(
+            Observable.Return(sessionProvider.Session),
+            Observable.FromEvent<ArchipelagoSession?>(h => sessionProvider.OnSessionChanged += h, h => sessionProvider.OnSessionChanged -= h)
+        ).DistinctUntilChanged().ToProperty(this, x => x.Session);
+        _hasSession = this.WhenAnyValue(x => x.Session).Select(x => x != null)
+            .ToProperty(this, x => x.HasSession);
+    }
 }
