2daddedbd3828d286f6adea0845ec5ed7e6f4c33
Hussein Farran <hmfarran@gmail.com>
2026-02-14 23:45:04 -0500

    Add ability to send chat messages from log view. (#69)
    * Add ability to send chat messages from log view.
    
    * Make chat a bit more aesthetically pleasing and send on enter
    
    * Minor code nitpicks
    
    ---------
    
    Co-authored-by: Sean Dempsey <dempsey.sean@outlook.com>

--- ArchipelagoDebugClient.Core/ViewModels/MessageLogViewModel.cs
+++ ArchipelagoDebugClient.Core/ViewModels/MessageLogViewModel.cs
@@ -1,9 +1,11 @@
-﻿using Archipelago.MultiClient.Net;
+﻿using System.Collections.ObjectModel;
+using System.Reactive;
+using Archipelago.MultiClient.Net;
 using Archipelago.MultiClient.Net.MessageLog.Messages;
 using ArchipelagoDebugClient.Models;
 using ArchipelagoDebugClient.Services;
 using Avalonia.Threading;
-using System.Collections.ObjectModel;
+using ReactiveUI;
 
 namespace ArchipelagoDebugClient.ViewModels;
 
@@ -11,8 +13,19 @@ public class MessageLogViewModel : ViewModelBase
 {
     public ObservableCollection<BindableMessage> Messages { get; } = [];
 
+    private string _chatMessage = "";
+    public string ChatMessage
+    {
+        get => _chatMessage;
+        set => this.RaiseAndSetIfChanged(ref _chatMessage, value);
+    }
+
+    public ReactiveCommand<Unit, Unit> SendMessageCommand { get; }
+
     public MessageLogViewModel(SessionProvider sessionProvider) : base(sessionProvider)
     {
+        SendMessageCommand = ReactiveCommand.Create(SendMessage, this.WhenAnyValue(x => x.HasSession));
+
         sessionProvider.OnSessionChanged += OnSessionChanged;
     }
 
@@ -31,5 +44,14 @@ public class MessageLogViewModel : ViewModelBase
     private void OnMessageRecieved(LogMessage message)
     {
         Dispatcher.UIThread.Invoke(() => Messages.Add(new BindableMessage(message)));
+    }
+
+    private void SendMessage()
+    {
+        if (!string.IsNullOrEmpty(ChatMessage))
+        {
+            Session!.Say(ChatMessage);
+        }
+        ChatMessage = string.Empty;
     }
 }
